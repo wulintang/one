3f63d4fa8073eabd7a5be7b010f91deeed2fc504
南通伍林堂文化传播有限公司 <15062733214@qq.com>
2022-05-13 16:44:34 +0800

    伍林堂

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from './link';
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div className="container">
+      <p>基于 <a href="https://uptimerobot.com/" target="_blank">UptimeRobot</a> 接口制作，检测频率 5 分钟</p>
+      <p>© <a href="https://www.wulintang.cn/" target="_blank">南通伍林堂文化传播有限公司</a> 版权所有</p>  
+      <p>本网站由 <a href="https://www.ccwaj.cn/" target="_blank" >云之道数据</a> 提供云计算服务</p>
+      <p><a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32061202001006">苏公网安备32061202001006号</a></p>
+      <p><a href="http://beian.miit.gov.cn/" target="_blank" rel="nofollow">苏ICP备2021008246号-1</a></p>
+      <p><a target="_blank" rel="nofollow" href="https://www.wulintang.xin/202004071.shtml"><img border="0" src="https://www.abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba.cn/yun/yun.png"/></a></p>
+      <p><a target="_blank" title="51la网站统计" href="https://v6.51.la/land/JgfK8L92UDczJx8b"><img src="https://sdk.51.la/icon/1-4.png"/></a></p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
